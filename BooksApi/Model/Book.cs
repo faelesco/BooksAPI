421b33ceab1833f81737ce58a8e89965de418ad4
Raphael Lelis <raphalelis@outlookc.com>
2022-10-27 16:47:20 -0300

    Create endpoint book

--- BooksApi/Model/Book.cs
+++ BooksApi/Model/Book.cs
@@ -2,7 +2,7 @@
 {
     public class Book
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Autor { get; set; }
         public string Description { get; set; }
         public string Title { get; set; }
